e6c980dfaf5391d1050b24e12ffb475894623461
Vishal Adhithya <58595594+terminal-guy@users.noreply.github.com>
2025-04-24 19:27:25 +0900

    Add files via upload

---
+++ AutomaticClock/AutomaticClock/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnContinue = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.buttonExit = New System.Windows.Forms.Button()
+        Me.btnCatch = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnContinue
+        '
+        Me.btnContinue.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnContinue.Location = New System.Drawing.Point(492, 138)
+        Me.btnContinue.Name = "btnContinue"
+        Me.btnContinue.Size = New System.Drawing.Size(87, 39)
+        Me.btnContinue.TabIndex = 0
+        Me.btnContinue.Text = "Continue"
+        Me.btnContinue.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.LightCoral
+        Me.Label1.Font = New System.Drawing.Font("Arial", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(68, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 72)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Yellow
+        Me.Label2.Font = New System.Drawing.Font("Arial", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(173, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 72)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "?"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Label3.Font = New System.Drawing.Font("Arial", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(282, 61)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 72)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "?"
+        '
+        'buttonExit
+        '
+        Me.buttonExit.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.buttonExit.Location = New System.Drawing.Point(492, 221)
+        Me.buttonExit.Name = "buttonExit"
+        Me.buttonExit.Size = New System.Drawing.Size(87, 38)
+        Me.buttonExit.TabIndex = 6
+        Me.buttonExit.Text = "Exit"
+        Me.buttonExit.UseVisualStyleBackColor = True
+        '
+        'btnCatch
+        '
+        Me.btnCatch.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnCatch.Location = New System.Drawing.Point(492, 61)
+        Me.btnCatch.Name = "btnCatch"
+        Me.btnCatch.Size = New System.Drawing.Size(87, 32)
+        Me.btnCatch.TabIndex = 7
+        Me.btnCatch.Text = "Catch"
+        Me.btnCatch.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(68, 212)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 23)
+        Me.Label4.TabIndex = 8
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.AutomaticClock.My.Resources.Resources.coinimage
+        Me.PictureBox1.Location = New System.Drawing.Point(127, 238)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(315, 140)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnCatch)
+        Me.Controls.Add(Me.buttonExit)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnContinue)
+        Me.Name = "Form1"
+        Me.Text = "Yetheendranadhan Vishal Adhithya"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnContinue As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents buttonExit As Button
+    Friend WithEvents btnCatch As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ AutomaticClock/AutomaticClock/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+
+    Private Counter As Integer = 0
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.Text = Int(Rnd() * 10)
+        Label2.Text = Int(Rnd() * 10)
+        Label3.Text = Int(Rnd() * 10)
+
+    End Sub
+
+    Private Sub btnCatch_Click(sender As Object, e As EventArgs) Handles btnCatch.Click
+
+        Dim X, Y, Z As Integer
+
+        Timer1.Enabled = False
+
+
+        X = Label1.Text
+        Y = Label2.Text
+        Z = Label3.Text
+
+        If (X = Y) Or (X = Z) Or (Y = Z) Then
+            Beep()
+            Label4.Text = "You win!"
+            PictureBox1.Visible = True
+        End If
+    End Sub
+
+
+
+    Private Sub btnContinue_Click(sender As Object, e As EventArgs) Handles btnContinue.Click
+        Label4.Text = ""
+        PictureBox1.Visible = False
+        Timer1.Enabled = True
+        Counter = Counter + 1
+
+    End Sub
+
+    Private Sub buttonExit_Click(sender As Object, e As EventArgs) Handles buttonExit.Click
+        MessageBox.Show("You had " & Counter & " Wins!")
+        End
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_2(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
