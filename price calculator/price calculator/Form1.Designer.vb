87aa16910e4f9ddd00b9354dbec6fb75da83590c
Vishal Adhithya <58595594+terminal-guy@users.noreply.github.com>
2025-05-08 08:17:03 +0530

    Add files via upload

---
+++ price calculator/price calculator/Form1.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtquantity = New System.Windows.Forms.TextBox()
+        Me.txtprice = New System.Windows.Forms.TextBox()
+        Me.txtname = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblttl = New System.Windows.Forms.Label()
+        Me.lbltax = New System.Windows.Forms.Label()
+        Me.lblsubttl = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btncalculate = New System.Windows.Forms.Button()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtquantity)
+        Me.GroupBox1.Controls.Add(Me.txtprice)
+        Me.GroupBox1.Controls.Add(Me.txtname)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Font = New System.Drawing.Font("MS UI Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(152, 50)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(258, 130)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Input Data;"
+        '
+        'txtquantity
+        '
+        Me.txtquantity.Location = New System.Drawing.Point(138, 67)
+        Me.txtquantity.Name = "txtquantity"
+        Me.txtquantity.Size = New System.Drawing.Size(100, 20)
+        Me.txtquantity.TabIndex = 7
+        '
+        'txtprice
+        '
+        Me.txtprice.Location = New System.Drawing.Point(138, 38)
+        Me.txtprice.Name = "txtprice"
+        Me.txtprice.Size = New System.Drawing.Size(100, 20)
+        Me.txtprice.TabIndex = 6
+        '
+        'txtname
+        '
+        Me.txtname.Location = New System.Drawing.Point(138, 12)
+        Me.txtname.Name = "txtname"
+        Me.txtname.Size = New System.Drawing.Size(100, 20)
+        Me.txtname.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Yellow
+        Me.Label4.Location = New System.Drawing.Point(6, 74)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Quantity:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Yellow
+        Me.Label2.Location = New System.Drawing.Point(6, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(103, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Product Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Yellow
+        Me.Label3.Location = New System.Drawing.Point(6, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "price:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblttl)
+        Me.GroupBox2.Controls.Add(Me.lbltax)
+        Me.GroupBox2.Controls.Add(Me.lblsubttl)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Font = New System.Drawing.Font("MS UI Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(152, 250)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(258, 109)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Output;"
+        '
+        'lblttl
+        '
+        Me.lblttl.AutoSize = True
+        Me.lblttl.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblttl.Location = New System.Drawing.Point(168, 76)
+        Me.lblttl.Name = "lblttl"
+        Me.lblttl.Size = New System.Drawing.Size(14, 13)
+        Me.lblttl.TabIndex = 8
+        Me.lblttl.Text = "?"
+        '
+        'lbltax
+        '
+        Me.lbltax.AutoSize = True
+        Me.lbltax.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lbltax.Location = New System.Drawing.Point(168, 48)
+        Me.lbltax.Name = "lbltax"
+        Me.lbltax.Size = New System.Drawing.Size(14, 13)
+        Me.lbltax.TabIndex = 7
+        Me.lbltax.Text = "?"
+        '
+        'lblsubttl
+        '
+        Me.lblsubttl.AutoSize = True
+        Me.lblsubttl.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblsubttl.Location = New System.Drawing.Point(168, 25)
+        Me.lblsubttl.Name = "lblsubttl"
+        Me.lblsubttl.Size = New System.Drawing.Size(14, 13)
+        Me.lblsubttl.TabIndex = 6
+        Me.lblsubttl.Text = "?"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Yellow
+        Me.Label7.Location = New System.Drawing.Point(6, 76)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(45, 13)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Total:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Yellow
+        Me.Label6.Location = New System.Drawing.Point(6, 48)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 13)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Tax:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Yellow
+        Me.Label5.Location = New System.Drawing.Point(6, 25)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(69, 13)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "SubTotal:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(136, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(330, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Input Product Name, Quanitity, and Price "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 19)
+        Me.TextBox1.TabIndex = 3
+        '
+        'btncalculate
+        '
+        Me.btncalculate.Location = New System.Drawing.Point(225, 201)
+        Me.btncalculate.Name = "btncalculate"
+        Me.btncalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btncalculate.TabIndex = 4
+        Me.btncalculate.Text = "Calculate "
+        Me.btncalculate.UseVisualStyleBackColor = True
+        '
+        'btnexit
+        '
+        Me.btnexit.Location = New System.Drawing.Point(335, 201)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(75, 23)
+        Me.btnexit.TabIndex = 5
+        Me.btnexit.Text = "Exit "
+        Me.btnexit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btncalculate)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "kuldeep APU"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtquantity As TextBox
+    Friend WithEvents txtprice As TextBox
+    Friend WithEvents txtname As TextBox
+    Friend WithEvents lblttl As Label
+    Friend WithEvents lbltax As Label
+    Friend WithEvents lblsubttl As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btncalculate As Button
+    Friend WithEvents btnexit As Button
+End Class
---
+++ price calculator/price calculator/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Security.Cryptography
+
+Public Class Form1
+
+
+    Private Sub btncalculate_Click(sender As Object, e As EventArgs) Handles btncalculate.Click
+'1. Declare Variables and Constants
+'Note: Variable names use 'Camel Casing'
+Dim productName As String
+Dim price As Double
+Dim quantity As Integer
+Dim subTotal, tax, total As Double
+Const taxRate As Double = 0.1 ' 10% Consumption Tax
+
+'2. Perform Data Input
+'Note the implicit String conversions
+productName = txtName.Text
+price = txtPrice.Text
+quantity = txtQuantity.Text
+        ' 3. Perform Calculations subTotal - quantity * price
+        subTotal = quantity * price
+        tax = subTotal * taxRate
+        total = subTotal + tax
+        ' 4. Formatted Output
+        ' For efficiency, we define a single format string for all outputs
+        Dim myFormat As String = "$#,##0.00"
+        ' This format string specifies:
+        '(1) A leading dollar sign,
+        '(2) Commas every 3rd digit,
+        '(3) At least one leading digit, And
+        '(4) Two trazling digits after the decimal place.
+        'Now, we apply it to all 3 outputs
+        lblsubttl.Text = Format(subTotal, myFormat)
+        lbltax.Text = Format(tax, myFormat)
+        lblttl.Text = Format(total, myFormat)
+    End Sub
+
+    Private Sub btnexit_Click(sender As Object, e As EventArgs) Handles btnexit.Click
+        End
+
+    End Sub
+End Class
