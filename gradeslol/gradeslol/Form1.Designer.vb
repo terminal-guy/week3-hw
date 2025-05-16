15ca4a80cf8ee1d7b54196a8864e053d7865395d
Vishal Adhithya <58595594+terminal-guy@users.noreply.github.com>
2025-05-16 09:41:27 +0900

    new week hw

---
+++ gradeslol/gradeslol/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.btnIfThen = New System.Windows.Forms.Button()
+        Me.btnSelectCase = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(150, 140)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Student Grade"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(150, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter your score"
+        '
+        'txtScore
+        '
+        Me.txtScore.Location = New System.Drawing.Point(368, 69)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(100, 20)
+        Me.txtScore.TabIndex = 2
+        '
+        'btnIfThen
+        '
+        Me.btnIfThen.Location = New System.Drawing.Point(103, 227)
+        Me.btnIfThen.Name = "btnIfThen"
+        Me.btnIfThen.Size = New System.Drawing.Size(120, 23)
+        Me.btnIfThen.TabIndex = 3
+        Me.btnIfThen.Text = "If... Then... Else..."
+        Me.btnIfThen.UseVisualStyleBackColor = True
+        '
+        'btnSelectCase
+        '
+        Me.btnSelectCase.Location = New System.Drawing.Point(255, 227)
+        Me.btnSelectCase.Name = "btnSelectCase"
+        Me.btnSelectCase.Size = New System.Drawing.Size(75, 23)
+        Me.btnSelectCase.TabIndex = 4
+        Me.btnSelectCase.Text = "Select Case"
+        Me.btnSelectCase.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(403, 227)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(400, 140)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(19, 13)
+        Me.lblResult.TabIndex = 6
+        Me.lblResult.Text = "??"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(639, 395)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSelectCase)
+        Me.Controls.Add(Me.btnIfThen)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Yetheendranadhan Vishal Adhithya APU"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtScore As TextBox
+    Friend WithEvents btnIfThen As Button
+    Friend WithEvents btnSelectCase As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblResult As Label
+End Class
---
+++ gradeslol/gradeslol/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+
+    Dim lastScore As Integer
+
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub btnIfThen_Click(sender As Object, e As EventArgs) Handles btnIfThen.Click
+        Dim score = CType(txtScore.Text, Integer)
+        lblResult.BackColor = Color.LightGreen
+
+        If (score >= 90) Then
+            lblResult.Text = "A+"
+            MessageBox.Show("Congratulations!!", "MyStudentGrader")
+        ElseIf (score >= 80) Then
+            lblResult.Text = "A"
+        ElseIf (score >= 70) Then
+            lblResult.Text = "B"
+        ElseIf (score >= 60) Then
+            lblResult.Text = "C"
+        Else
+            lblResult.Text = "F"
+            lblResult.BackColor = Color.IndianRed
+        End If
+    End Sub
+
+
+    Private Sub btnSelectCase_Click(sender As Object, e As EventArgs) Handles btnSelectCase.Click
+        Dim score = CType(txtScore.Text, Integer)
+        lblResult.BackColor = Color.LightGreen
+
+        Select Case score
+            Case Is >= 90
+                lblResult.Text = "A+"
+                MessageBox.Show("Congratulations!!", "MyStudentGrader")
+            Case Is >= 80
+                lblResult.Text = "A"
+            Case Is >= 70
+                lblResult.Text = "B"
+            Case Is >= 60
+                lblResult.Text = "C"
+            Case Else
+                lblResult.Text = "F"
+                lblResult.BackColor = Color.IndianRed
+        End Select
+    End Sub
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+
+
+
+
+
+End Class
