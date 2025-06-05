dd5bb476735bdaed14f712b68f11d317d42f31c9
Vishal Adhithya <58595594+terminal-guy@users.noreply.github.com>
2025-06-05 11:55:44 +0900

    Add files via upload

---
+++ DayAction/DayAction/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.trkHour = New System.Windows.Forms.TrackBar()
+        Me.lblState = New System.Windows.Forms.Label()
+        CType(Me.trkHour, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'trkHour
+        '
+        Me.trkHour.Location = New System.Drawing.Point(59, 31)
+        Me.trkHour.Maximum = 23
+        Me.trkHour.Name = "trkHour"
+        Me.trkHour.Size = New System.Drawing.Size(432, 45)
+        Me.trkHour.TabIndex = 0
+        '
+        'lblState
+        '
+        Me.lblState.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblState.Location = New System.Drawing.Point(73, 120)
+        Me.lblState.Name = "lblState"
+        Me.lblState.Size = New System.Drawing.Size(408, 42)
+        Me.lblState.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(536, 171)
+        Me.Controls.Add(Me.lblState)
+        Me.Controls.Add(Me.trkHour)
+        Me.Name = "Form1"
+        Me.Text = "Yetheendranadhan Vishal Adhithya 12423155"
+        CType(Me.trkHour, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents trkHour As TrackBar
+    Friend WithEvents lblState As Label
+End Class
---
+++ DayAction/DayAction/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblState.Click
+
+    End Sub
+
+    Public Property Hour As Integer
+        Get
+            Return trkHour.Value
+        End Get
+        Set(value As Integer)
+
+            trkHour.Value = value
+
+            Dim statusText As String
+            statusText = "At " & value & ":00, Albert is " & PredictAction(value).ToString()
+
+            Me.lblState.Text = statusText
+
+        End Set
+    End Property
+
+
+    Public CurrentState As DayAction
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Hour = Date.Now.Hour
+    End Sub
+
+    Private Sub trkHour_Scroll(sender As Object, e As EventArgs) Handles trkHour.Scroll
+        Me.Hour = trkHour.Value
+    End Sub
+
+
+
+    Private Function PredictAction(ByVal time As Integer) As DayAction
+        Dim action As DayAction
+
+        Select Case time
+            Case 0 To 6
+                action = DayAction.Asleep
+            Case 7
+                action = DayAction.GettingReadyForWork
+            Case 8
+                action = DayAction.TravellingToWork
+            Case 9 To 11, 13 To 17
+                action = DayAction.AtWork
+            Case 12
+                action = DayAction.AtLunch
+            Case 18
+                action = DayAction.TravellingFromWork
+            Case 19 To 21
+                action = DayAction.RelaxingWithFriends
+            Case Else
+                action = DayAction.PreparingForSleep
+        End Select
+
+        Me.CurrentState = action
+
+        Return action
+    End Function
+
+
+
+
+End Class
+
+
+Public Enum DayAction As Integer
+    Asleep = 0
+    GettingReadyForWork = 1
+    TravellingToWork = 2
+    AtWork = 3
+    AtLunch = 4
+    TravellingFromWork = 5
+    RelaxingWithFriends = 6
+    PreparingForSleep = 7
+End Enum
+
+
+
